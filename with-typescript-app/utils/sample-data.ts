f87223377e1bd9f5d1ce8dec03ff82a2c4a2fd65
geonwoomun <ansejrrhkd@naver.com>
2020-06-08 15:31:00 +0900

    next typesscript 맛보기

---
+++ with-typescript-app/utils/sample-data.ts
@@ -0,0 +1,13 @@
+import { User } from '../interfaces';
+
+/** Dummy user data. */
+export const sampleUserData: User[] = [
+  { id: 101, name: 'Alice' },
+  { id: 102, name: 'Bob' },
+  { id: 103, name: 'Caroline' },
+  { id: 104, name: 'Dave' },
+  { id: 105, name: 'Dave' },
+  { id: 106, name: 'Dave' },
+  { id: 107, name: 'Dave' },
+  { id: 108, name: 'Dave' },
+];
