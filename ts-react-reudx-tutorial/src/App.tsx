351156f6c6d341e0a6aadef9e53d646bccd6afc3
geonwoomun <ansejrrhkd@naver.com>
2020-06-19 14:46:47 +0900

    velopert ts redux-thunk

--- ts-react-reudx-tutorial/src/App.tsx
+++ ts-react-reudx-tutorial/src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import TodoInsert from './components/TodoInsrt';
-import TodoList from './components/TodoList';
-import Counter from './components/Counter';
+// import TodoInsert from './components/TodoInsrt';
+// import TodoList from './components/TodoList';
+// import Counter from './components/Counter';
+import GithubProfileLoader from './containers/GithubProfileLoader';
 
 function App() {
   return (
-    <>
-      <TodoInsert />
-      <TodoList />
-      {/* <Counter /> */}
-    </>
+    // <>
+    //   {/* <TodoInsert />
+    //   <TodoList /> */}
+    //   {/* <Counter /> */}
+
+    // </>
+    <GithubProfileLoader />
   );
 }
 
