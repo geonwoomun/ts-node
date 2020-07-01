f2ab6cf4512276d674e7475083c15035912f6dbf
geonwoomun <38036598+geonwoomun@users.noreply.github.com>
2020-07-01 22:22:19 +0900

    스토리북, 이모션 작업

--- storybook-tutorial/.storybook/main.js
+++ storybook-tutorial/.storybook/main.js
@@ -14,6 +14,19 @@ module.exports = {
           loader: require.resolve('babel-loader'),
           options: {
             presets: [['react-app', { flow: false, typescript: true }]],
+            plugins: [
+              [
+                require.resolve('babel-plugin-named-asset-import'),
+                {
+                  loaderMap: {
+                    svg: {
+                      ReactComponent:
+                        '@svgr/webpack?-svgo,+titleProp,+ref![path]',
+                    },
+                  },
+                },
+              ],
+            ],
           },
         },
         require.resolve('react-docgen-typescript-loader'),
